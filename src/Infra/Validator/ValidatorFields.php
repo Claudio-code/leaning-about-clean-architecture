e9137afd63ac2944d72604603e54bc48fd905cff
claudio-code <suporte2424@outlook.com>
2022-03-09 22:04:55 -0300

    added sub method do validate cpf pattern

--- src/Infra/Validator/ValidatorFields.php
+++ src/Infra/Validator/ValidatorFields.php
@@ -14,22 +14,27 @@ class ValidatorFields
 
     public static function validateCpf(string $cpf): void
     {
-        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
-        if (strlen($cpf) != 11) {
-            throw new \InvalidArgumentException('Invalid cpf');
+        $cpfToValidate = preg_replace( '/[^0-9]/is', '', $cpf );
+        if (strlen($cpfToValidate) != 11) {
+            throw new \InvalidArgumentException('Invalid cpf length');
         }
 
-        if (preg_match('/(\d)\1{10}/', $cpf)) {
-            throw new \InvalidArgumentException('Invalid cpf');
+        if (preg_match('/(\d)\1{10}/', $cpfToValidate)) {
+            throw new \InvalidArgumentException('Invalid cpf, why repeated values');
         }
 
+        self::validateCpfPattern($cpfToValidate);
+    }
+
+    private static function validateCpfPattern(string $cpf): void
+    {
         for ($t = 9; $t < 11; $t++) {
             for ($d = 0, $c = 0; $c < $t; $c++) {
                 $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
             if ($cpf[$c] != $d) {
-                throw new \InvalidArgumentException('Invalid cpf');
+                throw new \InvalidArgumentException('Invalid cpf pattern');
             }
         }
     }
