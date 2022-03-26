cb52b3c989da0d79e4b0cbf4e73f2ced925ad827
claudio-code <suporte2424@outlook.com>
2022-03-25 21:30:10 -0300

    added test do validate email

---
+++ tests/Unit/Infra/ObjectValue/EmailTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Infra\ObjectValue;
+
+use Claudio\LeaningAboutCleanArchitecture\Infra\ObjectValue\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    /** @test */
+    public function shouldCreateEmailObject(): void
+    {
+        $emailValid = 'nome@gmail.com';
+        $email = new Email($emailValid);
+
+        self::assertEquals($emailValid, $email);
+    }
+
+    /** @test */
+    public function shouldReturnExceptionIfInvalidEmail(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('Invalid e-mail address');
+        $emailInvalid = '2eofepqo@gmaidwqdq';
+        new Email($emailInvalid);
+    }
+}
